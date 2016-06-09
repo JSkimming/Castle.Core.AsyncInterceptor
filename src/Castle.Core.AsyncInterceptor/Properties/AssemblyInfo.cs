dbd898ca7092731f76326022736684ba52b51522
James Skimming <JSkimming@users.noreply.github.com>
2016-06-09 14:57:45 +0100

    Added the initial empty project and build

---
+++ src/Castle.Core.AsyncInterceptor/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Castle.Core.AsyncInterceptor")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("51ba6816-5fdb-4ae7-a473-4c5cf1c1faf4")]
