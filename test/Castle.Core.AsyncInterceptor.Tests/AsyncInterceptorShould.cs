0278027355b4e0854e69f29116227e6ecf0ba395
James Skimming <JSkimming@users.noreply.github.com>
2016-06-10 15:27:43 +0100

    Added the unit test project

---
+++ test/Castle.Core.AsyncInterceptor.Tests/AsyncInterceptorShould.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2016 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Castle.DynamicProxy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Ploeh.AutoFixture;
+    using Xunit;
+
+    public class AsyncInterceptorShould
+    {
+        [Fact]
+        public void Implement_IInterceptor()
+        {
+            var fixture = new Fixture().Customize(new AsyncInterceptorCustomization());
+            var sut = fixture.Create<AsyncInterceptor>();
+
+            Assert.IsAssignableFrom<IInterceptor>(sut);
+        }
+    }
+}
---
+++ test/Castle.Core.AsyncInterceptor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2016 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Castle.Core.AsyncInterceptor.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+////[assembly: Guid("dfdf603a-5adb-4060-9d7c-17ba4f61982a")]
