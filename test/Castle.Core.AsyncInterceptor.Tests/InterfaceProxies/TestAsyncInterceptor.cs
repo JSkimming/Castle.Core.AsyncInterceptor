5af47983799e3bd6455188c8cedd16d1124b9bc7
Bruno <bruno.blank@offerta.se>
2018-02-25 22:11:55 +0100

    Added Proxy Generator Extension for: CreateInterfaceProxyWithoutTarget

--- test/Castle.Core.AsyncInterceptor.Tests/InterfaceProxies/TestAsyncInterceptor.cs
+++ test/Castle.Core.AsyncInterceptor.Tests/InterfaceProxies/TestAsyncInterceptor.cs
@@ -11,22 +11,28 @@ namespace Castle.DynamicProxy.InterfaceProxies
     {
         private readonly ICollection<string> _log;
 
-        public TestAsyncInterceptor(List<string> log)
+        private readonly bool _shouldProceed;
+
+        public TestAsyncInterceptor(List<string> log, bool shouldProceed = true)
         {
             _log = log ?? throw new ArgumentNullException(nameof(log));
+            _shouldProceed = shouldProceed;
         }
 
         public void InterceptAction(IActionInvocation invocation)
         {
             LogInterceptStart(invocation);
-            invocation.Proceed();
+            if (_shouldProceed)
+                invocation.Proceed();
             LogInterceptEnd(invocation);
         }
 
         public TResult InterceptFunction<TResult>(IFunctionInvocation<TResult> invocation)
         {
+            TResult result = default(TResult);
             LogInterceptStart(invocation);
-            TResult result = invocation.Proceed();
+            if (_shouldProceed)
+                result = invocation.Proceed();
             LogInterceptEnd(invocation);
             return result;
         }
@@ -34,15 +40,17 @@ namespace Castle.DynamicProxy.InterfaceProxies
         public async Task InterceptAsyncAction(IAsyncActionInvocation invocation)
         {
             LogInterceptStart(invocation);
-            Task task = invocation.ProceedAsync();
-            await task.ConfigureAwait(false);
+            if (_shouldProceed)
+                await invocation.ProceedAsync().ConfigureAwait(false);
             LogInterceptEnd(invocation);
         }
 
         public async Task<TResult> InterceptAsyncFunction<TResult>(IAsyncFunctionInvocation<TResult> invocation)
         {
+            TResult result = default(TResult);
             LogInterceptStart(invocation);
-            TResult result = await invocation.ProceedAsync().ConfigureAwait(false);
+            if (_shouldProceed)
+                result = await invocation.ProceedAsync().ConfigureAwait(false);
             LogInterceptEnd(invocation);
             return result;
         }
