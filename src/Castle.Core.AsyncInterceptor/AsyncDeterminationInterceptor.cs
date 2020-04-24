a82423288e98ed7788ff4452326f0825cb51812c
James Skimming <647706+JSkimming@users.noreply.github.com>
2020-04-24 23:04:37 +0100

    Updated copyright headers

--- src/Castle.Core.AsyncInterceptor/AsyncDeterminationInterceptor.cs
+++ src/Castle.Core.AsyncInterceptor/AsyncDeterminationInterceptor.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 James Skimming. All rights reserved.
+// Copyright (c) 2016-2020 James Skimming. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 namespace Castle.DynamicProxy
